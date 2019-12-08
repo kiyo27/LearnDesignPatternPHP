6ce4ba24349bfa8d2b828ac572c030a3e6593ad3
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2019-12-08 20:22:26 +0900

    add

---
+++ client.php
@@ -0,0 +1,28 @@
+<?php
+
+use Flyweight\ItemFactory;
+
+require('vendor/autoload.php');
+
+function dumpData($data) {
+    foreach($data as $object) {
+        echo $object->getName() . " ";
+        echo "商品番号" . $object->getCode() . " ";
+        echo $object->getPrice();
+    }
+}
+
+$factory = ItemFactory::getInstance('./Flyweight/data.txt');
+
+$items = [];
+$items[] = $factory->getItem('ABC001');
+$items[] = $factory->getItem('ABC002');
+$items[] = $factory->getItem('ABC003');
+
+if ($items[0] === $factory->getItem('ABC001')) {
+    echo "同一オブジェクトです\n";
+} else {
+    echo "同一のオブジェクトではありません\n";
+}
+
+dumpData($items);
