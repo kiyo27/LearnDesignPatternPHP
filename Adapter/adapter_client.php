d883c0e415b1d7c17ecca665fc48be71d97d1a25
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2019-12-28 15:56:20 +0900

    mod

--- Adapter/adapter_client.php
+++ Adapter/adapter_client.php
@@ -2,7 +2,7 @@
 
 use Adapter\DisplaySourceFileImple;
 
-require('vendor/autoload.php');
+require('../vendor/autoload.php');
 
 $show_file = new DisplaySourceFileImple('./Adapter/ShowFile.php');
 $show_file->display();
